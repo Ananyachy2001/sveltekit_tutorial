133981ea2605fdbdcb7c313c53331293d1e7eadc
Ananyachy2001 <ananyachy2001@gmail.com>
2024-07-03 16:43:56 +0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+let title = "Ananyo Gaming Guides"
+
+
+const updateTitle = () => {
+title = "something comepletely different"
+}
+</script>
+<div class="index">
+<h1>{title}</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet dignissimos sint optio odit cupiditate necessitatibus eaque nemo, dolorem fugiat.</p>
+<button on:click={updateTitle}>change title</button>
+
+<input type="text" bind:value={title}>
+</div>
+<style>
+.index{
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+}
+</style>
