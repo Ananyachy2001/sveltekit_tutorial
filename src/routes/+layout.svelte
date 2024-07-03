d0edc27796427499697fda80e0aa852a1d4dc3e4
Ananyachy2001 <ananyachy2001@gmail.com>
2024-07-03 17:37:08 +0600

    layout component add

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+import Title from "$lib/title.svelte";
+import '../styles/global.css'
+</script>
+
+
+<header>
+    <Title title="Pro Gaming Guides" />
+</header>
+
+<main>
+    <!-- page content -->
+     <slot></slot>
+</main>
+
+<footer>
+    <p>Copyright 2024 Pro Gaming Guides</p>
+</footer>
+
+<style>
+    header{
+        display: flex;
+        justify-content: center;
+    }
+
+    main{
+        max-width: 960px;
+        margin: 20px auto;
+    }
+    footer{
+        text-align: center;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
 let title = "Ananyo Gaming Guides"
 
-import Title from '$lib/title.svelte'
+
 
 const updateTitle = () => {
 title = "something comepletely different"
 }
 </script>
 
-<Title/>
+
 <div class="index">
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet dignissimos sint optio odit cupiditate necessitatibus eaque nemo, dolorem fugiat.</p>
 <button on:click={updateTitle}>change title</button>
